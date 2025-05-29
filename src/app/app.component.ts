ced807dab6c3a1ae9cc07b7d3518a31c32a56c0c
sergs1305 <sergs13@gmail.com>
2025-05-29 15:18:13 +0300

    Добавит компоненты favorites, login, offer.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+// import { RouterOutlet } from '@angular/router';
 import { PlacesListComponent } from './places-list/places-list.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, PlacesListComponent],
+  imports: [PlacesListComponent], // RouterOutlet
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'angular-template-project';
-
 }
