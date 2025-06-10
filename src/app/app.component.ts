046bd36fdf6bb09afe13d7d0437e049a809a179c
sergs1305 <sergs13@gmail.com>
2025-06-10 13:07:50 +0300

    Добавит outlet.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-// import { RouterOutlet } from '@angular/router';
-import { MainComponent } from "./main/main.component";
+import { RouterOutlet } from '@angular/router';
+// import { MainComponent } from "./main/main.component";
 
 @Component({
   selector: 'app-root',
-  imports: [MainComponent], // RouterOutlet
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
