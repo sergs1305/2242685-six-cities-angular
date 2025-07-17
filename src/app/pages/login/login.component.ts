57798d2f116ce6d95a25089c0bd6e33a7bf9f947
sergs1305 <sergs13@gmail.com>
2025-07-17 16:51:35 +0300

    Добавит форму регистрации пользователя (компонент login)

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ReactiveFormsModule, FormBuilder, Validators, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ReactiveFormsModule ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  loginSection!: FormGroup;
+  userEmail = '';
+  userPassword = '';
+
+  constructor(private fb: FormBuilder) { };
+
+  ngOnInit() {
+    this.loginSection = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+
+    this.loginSection.valueChanges.subscribe(values => {
+      this.userEmail = values.email;
+      this.userPassword = values.password;
+    });
+  }
 
 }
